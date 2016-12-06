017d4c6643eb7b38c953a8dc45e226b7ead1af69
Uniericuni <uniericuni@gmail.com>
2016-12-06 13:12:05 -0500

    [Init]

---
+++ run_red_varyk.m
@@ -0,0 +1,10 @@
+% run_red_varyk.m
+%
+%  504; jason corso
+%
+% Run the red stitching script, but vary the number of points used in the
+% fitting of the homography.
+
+kCorr = 10;
+run_red
+close all;
